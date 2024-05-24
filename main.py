8c0bad492a4ea875e0412fe0e39985dbbb3531ab
Blake Bradford <james.b.bradford@faa.gov>
2024-05-24 13:50:07 -0400

    chore: Add CORS middleware to allow requests from specific origin

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins=[
+    "https://young-beach-68721-491f33603242.herokuapp.com"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
